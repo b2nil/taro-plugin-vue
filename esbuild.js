46c0c8a7fd109f65b802b2ea0f30e613fdf9b5fe
Ken Ho <kentsu.ho@gmail.com>
2021-08-20 17:38:44 +0800

    build: add target platform and bundle external config

--- esbuild.js
+++ esbuild.js
@@ -1,10 +1,16 @@
 require('esbuild').buildSync({
   entryPoints: ['src/index.ts'],
+  platform: "node",
+  target: "node12",
   format: "cjs",
   minify: false,
   loader: {
     ".ts": "ts"
   },
+  bundle: true,
+  external: [
+    "@vitejs/plugin-vue"
+  ],
   outdir: "dist",
   tsconfig: 'tsconfig.json'
 })
